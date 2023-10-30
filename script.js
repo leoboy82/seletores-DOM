8ea4e5421553a45eb4f75959eaaed083d4b0a58e
Leandro França dos Santos <leandrofrancadossantos@gmail.com>
2023-10-30 19:32:06 -0300

    criando documento index e script

--- script.js
+++ script.js
@@ -1,1 +1,13 @@
-console.log("Olá mundo!");
+/*const titulo = document.getElementById("titulo")
+console.log(titulo)
+
+const textos = document.getElementsByClassName("texto-simples");
+console.log(textos);*/
+
+const segundoTitulo = document.querySelector("div h2");
+
+
+
+ 
+
+console.log(segundoTitulo);
